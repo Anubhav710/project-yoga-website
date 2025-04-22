78d1f2dcdcdbec39f873e35fbc8742ed4ccc6006
Anubhav Agrawal <anubhav@UTXOTechs-MacBook-Air.local>
2025-04-22 15:07:22 +0530

    updated website

--- src/components/section/Footer.tsx
+++ src/components/section/Footer.tsx
@@ -11,7 +11,7 @@ const Footer = () => {
             <input
               type="text"
               placeholder="Your e-mail"
-              className="bg-transparent outline-none border-b border-b-white placeholder:text-white w-full py-2"
+              className="bg-transparent text-white outline-none border-b border-b-white placeholder:text-white w-full py-2"
             />
           </div>
           <div className="w-full md:w-1/2 flex flex-col md:flex-row gap-10 md:gap-32 ml-5">
